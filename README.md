942859a18b33659e2d6136eb675bfd3743a620e9
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-10-25 01:39:43 -0400

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+
+Infomation Inspiration - https://starloopstudios.com/character-design-in-video-games-types-classes-and-characteristics/
--- script.js
+++ script.js
@@ -15,44 +15,75 @@ const adventurer = {
     companion: {
         name: "Leo",
         type: "Cat",
-        companion:{
-            name:`Frank`,
-            type:`Flea`,
-            belongings:[`small hat`, `sunglasses`]
-            
+        companion: {
+            name: `Frank`,
+            type: `Flea`,
+            belongings: [`small hat`, `sunglasses`]
+
         }
     },
-    roll (mod = 0) {
+    roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
-        }
     }
-    console.log(adventurer.companion.companion.belongings)
-    adventurer.roll() 
+}
+//     console.log(adventurer.companion.companion.belongings)
+//     adventurer.roll() 
+// Part 2: Class Fantasy
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
 
-    class Character {
-        constructor (name) {
-          this.name = name;
-          this.health = 100;
-          this.inventory = [];
-        }
-        roll(mod = 0){
- const result = Math.floor(Math.random() * 20) + 1 + mod;
-  console.log(`${this.name} rolled a ${result}.`)
-  }
-        }
-  
-      
 
-     // Re-create Robin using the Character class!
-      const robin = new Character("Robin");
-      robin.inventory = ["sword", "potion", "artifact"];
-      robin.companion = new Character("Leo");
-      robin.companion.type = "Cat";
-      robin.companion.companion = new Character("Frank");
-      robin.companion.companion.type = "Flea";
-      robin.companion.companion.inventory = ["small hat", "sunglasses"];
-      robin.roll();
-      robin.companion.roll();
-      robin.companion.companion.roll();
-      console.log(robin)
+
+//      // Re-create Robin using the Character class!
+//       const robin = new Character("Robin");
+//       robin.inventory = ["sword", "potion", "artifact"];
+//       robin.companion = new Character("Leo");
+//       robin.companion.type = "Cat";
+//       robin.companion.companion = new Character("Frank");
+//       robin.companion.companion.type = "Flea";
+//       robin.companion.companion.inventory = ["small hat", "sunglasses"];
+//       robin.roll();
+//       robin.companion.roll();
+//       robin.companion.companion.roll();
+//       console.log(robin)
+
+//Part 3: Class Features
+class Adventurer extends Character {
+    constructor(name, role, skill, weakness) {
+        super(name);
+        // Adventurers have specialized roles.
+        this.role = role;
+        ///Adventures have a special skill or talent
+        this.skill = skill;
+        ///Adventures have a weakness
+        this.weakness = weakness;
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+}
+class Companion extends Character {
+    constructor(name, type) {
+        super(name);
+        this.type = type;
+
+    }
+}
+const robin = new Adventurer(`Robin`,`Ninja`,`sword fighting/Martial Arts`,`Lack of weapons`)
+const leo = new Companion(`Leo`,`cat`,[`small hat, sunglasses`])
+console.log(robin,leo)
+
